147e0475ad7e8a68c628970858e2ef76805557b0
NonchalantGarage <hong.curtis@gmail.com>
2021-11-14 14:12:38 -0800

    add start and end game function, incorporated switch statement, added shop between ruonds

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,7 +7,7 @@ var playerMoney = 10;
 // Establish an array of enemy robots instead of doing it one by one
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 12; 
 
 // prints names of enemies
 console.log(enemyNames);
@@ -80,8 +80,16 @@ var fight = function(enemyName) {
   }
 };
 
-// for loop, to move onto the next robot to fight
-for (var i = 0; i < enemyNames.length; i++) {
+// variable to initilize game and reset player stats
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  
+
+  // for loop, to move onto the next robot to fight
+  for (var i = 0; i < enemyNames.length; i++) {
   // Move on to the next robot only if player is still alive
   if (playerHealth > 0) {
     // If true, window.alert, welcome to round increment round after the prior using the parent iterator 
@@ -98,10 +106,101 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // calls the fight using the variable as the argument which is looped in a for loo 
     fight(pickedEnemyName);
-  }
+
+    // check if player is capable of going to store and confirm if you there is another robot to even fight
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+      
+        if (storeConfirm) {
+          shop();
+        }
+      }
+    }
+
+
   // if all else fails, int this loop, and player health is falsy > 0. you die
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
+    }
   }
-}
+
+  endGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did!");
+
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + '.');
+  } else {
+    window.alert("You've lost your robot in battle!");
+  }
+
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm('Would you like to play again?');
+
+  if (playAgainConfirm) {
+    startGame();
+  } else {
+    window.alert('Thank you for playing Robot Gladiators! Come back soon!');
+  }
+};
+
+// go to shop between battles function
+var shop = function() {
+  // ask player what they'd like to do
+  var shopOptionPrompt = window.prompt(
+    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+  );
+
+  // use switch case to carry out action
+  switch (shopOptionPrompt) {
+    case 'REFILL':
+    case 'refill':
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+
+        // increase health and decrease money
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+    }
+    else {
+        window.alert("You don't have enough money!");
+    }
+      break;
+    case 'UPGRADE':
+    case 'upgrade':
+      if (playerMoney >= 7) {
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+        // increase attack and decrease money
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+    }
+    else {
+        window.alert("You don't have enough money!");
+    }
+      break;
+    case 'LEAVE':
+    case 'leave':
+      window.alert('Leaving the store.');
+
+      // do nothing, so function will end
+      break;
+    default:
+      window.alert('You did not pick a valid option. Try again.');
+
+      // call shop() again to force player to pick a valid option
+      shop();
+      break;
+  }
+};
+
+// start first game when page loads
+startGame();
+
+
+
